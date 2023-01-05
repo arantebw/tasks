8dd4a9e50a5364e8666e317922adc55e73af5851
Billy Arante <arantebillywilson@gmail.com>
2023-01-05 15:14:35 +0800

    feat(deploy): ✨ initial deployment

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>Tasks</h1>
+      <p>My personal SPA implementation of the Google Tasks app.</p>
+      <h2>WORK-IN-PROGRESS</h2>
+      <footer>Created by <a href="https://arantebw.github.io">Billy Arante</a></footer>
     </div>
   )
 }
